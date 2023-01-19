9a1c2156ad1008e6fec325c57282c76ec0efdfaf
JordanFH <jfernandezh19_2@unc.edu.pe>
2023-01-18 19:03:01 -0500

    version 1.1

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import os
 
 pathname = r"C:\Users\USER03-60\Downloads\PracticaSIG2"
@@ -19,3 +20,26 @@ line = "asfasfasf,dsadasddas,dasdsa"
 print line
 line = line.replace(",", ".")
 print line
+
+
+def remove_accent(input_string):
+    accents = u"áéíóúÁÉÍÓÚ"
+    change = "aeiouAEIOU"
+
+    i = 0
+    if len(input_string) > 0:
+        for s in input_string:
+            s = s.encode('utf-8', 'ignore').decode('utf-8')
+            for r in accents:
+                if r == s:
+                    input_string = input_string.replace(s, change[i])
+                i += 1
+            i = 0
+        return input_string
+
+    return ""
+
+
+s = u"áasfsafsaé"
+
+print remove_accent(s)
